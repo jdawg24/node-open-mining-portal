50b001dc6e4558a3991358775a3bf462e9b4c093
jdawg24 <78548840+jdawg24@users.noreply.github.com>
2022-05-01 15:17:15 +0100

    Update website.js

--- libs/website.js
+++ libs/website.js
@@ -95,7 +95,8 @@ module.exports = function(logger){
 
 
     //If an html file was changed reload it
-    watch('website', function(filename){
+    ///watch('website', function(filename){
+    watch('website', function(evt, filename){
         var basename = path.basename(filename);
         if (basename in pageFiles){
             console.log(filename);
